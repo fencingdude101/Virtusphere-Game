c26a31396f52367a1677eea9aaae63c42de9d536
fencingdude101 <mroctopi24@gmail.com>
2016-02-28 19:39:46 -0500

    Test Game Update

---
+++ Unity Projects/Work In Progress - Experimental Projects/Virtusphere Test/Assets/Player.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	[SerializeField]private float playerspeed;
+    private float playertranslation;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        movePlayer();
+	}
+
+    void movePlayer(){
+        calculatePlayerMovement();
+        
+        transform.Translate(0, 0, playertranslation);
+    }
+
+    void calculatePlayerMovement(){
+		playertranslation = Input.GetAxis("Vertical") * playerspeed * Time.deltaTime;
+    }
+}
